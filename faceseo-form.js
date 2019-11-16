aa507422e9b3247aced02e3d4e44d48797e07277
nguyenlan123 <57832645+nguyenlan123@users.noreply.github.com>
2019-11-16 21:22:54 +0700

    Add files via upload

--- faceseo-form.js
+++ faceseo-form.js
@@ -139,7 +139,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://faceseo.vn";
+	window.location="https://chothuecanhodepbinhthanh.blogspot.com/#";
 var linkhientai=window.location;
 if(getCookie(linkhientai)){
                 	
